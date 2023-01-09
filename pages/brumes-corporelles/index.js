c95f6ef63c77638a5bef370ebb799cf9585bb192
Vittorio <tonibusatta@gmail.com>
2023-01-09 16:07:21 +0400

    somes changes

---
+++ pages/brumes-corporelles/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function BodyMist() {
+
+  return (
+    <>
+    <section className="container">
+      <div className="container_content">
+        <h1>Brumes corporelles</h1>
+      </div>
+      <div className="marquee">
+        <p>Creative</p>
+        <p>Creative</p>
+      </div>
+    </section>
+    </>
+  );
+}
+
+export default BodyMist;
