9059d7752a5838813e38427e208e76d92942b490
Vittorio <tonibusatta@gmail.com>
2023-01-19 00:35:11 +0400

    somes changes

--- components/Menu.js
+++ components/Menu.js
@@ -1,4 +1,5 @@
 import gsap from "gsap";
+import Link from "next/link";
 // import Link from "next/link";
 import React, { useEffect, useRef, useState, openMenu } from "react";
 
@@ -132,7 +133,6 @@ function Menu({
     },
   ];
 
-
   return (
     <section
       style={{ background: color1 }}
@@ -151,9 +151,13 @@ function Menu({
               <div style={{ background: color2 }} className="bar"></div>
             </button>
           </div>
-          <ul className="menu_list" style={{ color: color2 }}>
-            <li className="menu_item">Accueil</li>
-            <li className="menu_item">Categories</li>
+          <ul className="menu_list">
+            <li className="menu_item">
+              <Link style={{ color: color2 }} href="/">Accueil</Link>
+            </li>
+            <li className="menu_item">
+              <Link style={{ color: color2 }} href="/">Categories</Link>
+            </li>
             <ul className="menu_sublist" style={{ color: backgroundColor }}>
               {menuArray.map((item, index) => {
                 return (
@@ -167,7 +171,9 @@ function Menu({
                 );
               })}
             </ul>
-            <li className="menu_item">Contact</li>
+            <li className="menu_item">
+              <Link style={{ color: color2 }} href="/">Contact</Link>
+            </li>
           </ul>
         </div>
         {menuArray.map((item, index) => {
@@ -188,10 +194,10 @@ function Menu({
                 >
                   <path
                     d="M0.791344 1.09303C0.724782 1.19389 0.739888 1.32792 0.827228 1.41143L4.23153 4.6665L0.827437 7.91384C0.74047 7.99681 0.724848 8.12991 0.790243 8.23076L0.944992 8.4694C0.98548 8.53183 1.05168 8.57299 1.12558 8.58167C1.19949 8.59035 1.27342 8.56566 1.32728 8.5143L5.17252 4.84764C5.22201 4.80045 5.25001 4.73505 5.25 4.66667C5.24999 4.59829 5.22197 4.5329 5.17247 4.48572L1.32472 0.819057C1.27111 0.767974 1.19762 0.743283 1.12404 0.751637C1.05047 0.759992 0.984382 0.800533 0.943594 0.862335L0.791344 1.09303Z"
-                    fill="#FFEEFA"
-                    stroke="#FFEEFA"
-                    stroke-width="0.5"
-                    stroke-linejoin="round"
+                    fill={backgroundColor}
+                    stroke={backgroundColor}
+                    strokeWidth="0.5"
+                    strokeLinejoin="round"
                   />
                 </svg>
                 <span style={{ color: backgroundColor }}>Retour</span>
